b55641e5b13f9a1b881d46b877835c7f62bbf448
Myla <mila.mishchenko@outlook.com>
2020-07-22 17:53:45 +0200

    Added Shaker Sort Algorithm and tests

---
+++ Algorithms.Sorting/Algorithms.Sorting.Tests/ShakerSortTest.cs
@@ -0,0 +1,130 @@
+﻿using Algorithms.Common;
+using NUnit.Framework;
+using System;
+
+namespace Algorithms.Sorting.Tests
+{
+    public class ShakerSortTest : BaseSortTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SortIntComparisonDefault()
+        {
+            //arrange
+            var countElements = 100;
+            var arrayInt = GenerateArrayInt(countElements);
+            //var arrayInt = new int[10] {5,2,1,3,9,0,4,6,8,7 };
+            var arrayIntExpected = (int[])arrayInt.Clone();
+            Array.Sort<int>(arrayIntExpected, (a, b) => a.CompareTo(b));
+
+            var shakerSort = new ShakerSort<int>();
+
+            //act
+            shakerSort.Sort(arrayInt);
+
+            //assert
+            var result = ArraysAreEqual<int>(arrayInt, arrayIntExpected);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SortIntIncreasing()
+        {
+            //arrange
+            var countElements = 100;
+            var arrayInt = GenerateArrayInt(countElements);
+            var arrayIntExpected = (int[])arrayInt.Clone();
+            Array.Sort<int>(arrayIntExpected, (a, b) => a.CompareTo(b));
+
+            var shakerSort = new ShakerSort<int>();
+
+            //act
+            shakerSort.Sort(arrayInt, SortOrder.Increasing);
+
+            //assert
+            var result = ArraysAreEqual<int>(arrayInt, arrayIntExpected);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SortIntDecreasing()
+        {
+            //arrange
+            var countElements = 100;
+            var arrayInt = GenerateArrayInt(countElements);
+            var arrayIntExpected = (int[])arrayInt.Clone();
+            Array.Sort<int>(arrayIntExpected, (a, b) => b.CompareTo(a));
+
+            var shakerSort = new ShakerSort<int>();
+
+            //act
+            shakerSort.Sort(arrayInt, SortOrder.Decreasing);
+
+            //assert
+            var result = ArraysAreEqual<int>(arrayInt, arrayIntExpected);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SortStringComparisonDefault()
+        {
+            //arrange
+            var arrayString = GenerateArrayString();
+
+            var arrayStringExpected = (string[])arrayString.Clone();
+            Array.Sort<string>(arrayStringExpected, (a, b) => a.CompareTo(b));
+
+            var shakerSort = new ShakerSort<string>();
+
+            //act
+            shakerSort.Sort(arrayString);
+
+            //assert
+            var result = ArraysAreEqual<string>(arrayString, arrayStringExpected);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SortStringIncreasing()
+        {
+            //arrange
+            var arrayString = GenerateArrayString();
+
+            var arrayStringExpected = (string[])arrayString.Clone();
+            Array.Sort<string>(arrayStringExpected, (a, b) => a.CompareTo(b));
+
+            var shakerSort = new ShakerSort<string>();
+
+            //act
+            shakerSort.Sort(arrayString, SortOrder.Increasing);
+
+            //assert
+            var result = ArraysAreEqual<string>(arrayString, arrayStringExpected);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SortStringDecreasing()
+        {
+            //arrange
+            var arrayString = GenerateArrayString();
+
+            var arrayStringExpected = (string[])arrayString.Clone();
+            Array.Sort<string>(arrayStringExpected, (a, b) => b.CompareTo(a));
+
+            var shakerSort = new ShakerSort<string>();
+
+            //act
+            shakerSort.Sort(arrayString, SortOrder.Decreasing);
+
+            //assert
+            var result = ArraysAreEqual<string>(arrayString, arrayStringExpected);
+            Assert.IsTrue(result);
+        }
+
+    }
+}
---
+++ Algorithms.Sorting/Algorithms.Sorting/ShakerSort.cs
@@ -0,0 +1,50 @@
+﻿using Algorithms.Common;
+using System;
+
+namespace Algorithms.Sorting
+{
+    public class ShakerSort<T> : AlgotithmBase<T> where T : IComparable
+    {
+        private static string algorithmName = "Shaker Sort Algorithm";
+
+        public ShakerSort() : base(algorithmName)
+        {
+        }
+
+        public override void Sort(T[] array, SortOrder sortOrder)
+        {
+            Sort(array, Compare(sortOrder));
+        }
+
+        public override void Sort(T[] array, Comparison<T> comparison = null)
+        {
+            if (comparison == null)
+                comparison = comparisonDefault;
+
+            int firstIndex = 0;
+            int lastIndex = array.Length - 1;
+
+            while (firstIndex < lastIndex)
+            {
+                for (int i = firstIndex; i < lastIndex; i++)
+                {
+                    if (comparison.Invoke(array[i], array[i + 1]) >= 1)
+                    {
+                        Swop(i + 1, i, array);
+                    }
+                }
+
+                for (int j = lastIndex - 1; j > firstIndex; j--)
+                {
+                    if (comparison.Invoke(array[j - 1], array[j]) >= 1)
+                    {
+                        Swop(j - 1, j, array);
+                    }
+                }
+
+                firstIndex++;
+                lastIndex--;
+            }
+        }
+    }
+}
